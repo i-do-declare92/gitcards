c39e6914f5e9596ca6d433f86b1bf77b4a92d1bd
Stephen Serio <steph_stephen@outlook.com>
2020-08-26 02:09:27 -0500

    Ian helped me fix my huge code mess.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import React from 'react';
-import Welcome from './component/welcome/Welcome';
-import { Button } from '@material-ui/core';
-import Card from './component/card/Card';
-import UserCard from './component/userCard/UserCard';
+import { Card, Image } from 'semantic-ui-react';
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Welcome />
-      <Button>
-        <Card />
-        <Button/>
-        </Button>
-    </div>
+class App extends React.Component {
+  state = {
+    user: {},
+    active: false,
+  }
+
+  handleToggle = () => {
+    fetch("https://api.github.com/users/someonestaring")
+      .then(response => {
+        return response.json()
+          .then(data => {
+            this.setState({ user: data });
+          })
+      }
   );
 
+  if(this.state.active === false) {
+  this.setState({ active: true})
+} else { this.setState({ active: false }) }
 }
+  render() {
+    if (this.state.active === true) {
+      
+      return (
+        <div className="App">
+          <button className="ui violet button" onClick={this.handleToggle}>Press Me</button>
+          <Card>
+            <Image src={this.state.user.avatar_url} alt="Git Picture" />
+            <Card.Content style={{ background: "rgba(20, 210, 200, 0.313)" }}>
+              <Card.Header>{this.state.user.name}</Card.Header>
+              <Card.Meta>{this.state.user.bio}</Card.Meta>
+              <Card.Description>Username: {this.state.user.login}</Card.Description>
+            </Card.Content>
+          </Card>
+        </div>
+      );
+    }
+    return (
 
+      <div className="App">
+        <button className="ui violet button" onClick={this.handleToggle}>
+          Press Me
+        </button>
+      </div>
+    );
+  }
+}
+// Ian Waddell (Brother) helped me fix my diasterious mess. xD
 export default App;
+// Ian Waddell (Brother) helped me fix my diasterious mess. xD
--- src/component/userCard/UserCard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UserCard = (props) => (
-    <div className="UserCard">
-        {props.children}
-    </div>
-);
-
-export default UserCard;
